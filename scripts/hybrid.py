2364495e98b9ea7949a18490955ec4a104c7b2b6
Jude Jackson Davis <j.davis@se21.qmul.ac.uk>
2025-04-23 18:47:44 +0100

    sync

--- scripts/hybrid.py
+++ scripts/hybrid.py
@@ -438,6 +438,8 @@ class TCPHybrid (Server):
         return True
 
     def receieve_handshake(self, addr : str, session_id : bytes, sym_key : str, peer_ident : bytes) -> bool:
+        # commit sym_key to peer_table
+        self.peer_table.update_user_s_key(peer_ident, sym_key)
         # message = rand|signatute(rand)
         message = bytearray()
         rand = r.randbytes(8) # 8 bytes of random data to sign

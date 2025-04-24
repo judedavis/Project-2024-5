132adeffb4fe04dfa220b561387962e2733ada0b
Jude Jackson Davis <j.davis@se21.qmul.ac.uk>
2025-04-24 16:22:41 +0100

    Finished join network protocol operation

--- scripts/hybrid.py
+++ scripts/hybrid.py
@@ -713,10 +713,10 @@ class TCPHybrid (Server):
     
     def receive_join_network(self, addr : str, session_id : bytes) -> bool:
         self._send_message(addr, self.port, MessageTypes.JOIN_NETWORK_ACK, session_id)
+        self.async_wait_event(MessageTypes.UPDATE_PEERS_FINAL_2, addr, session_id) # wait until the update peers function is complete
         self._client_response(addr) # expect the key exchange
         self._client_response(addr) # expect the handshake
         self._client_response(addr) # expect the update peers
-        self.async_wait_event(MessageTypes.UPDATE_PEERS_FINAL_2, addr, session_id) # wait until the update peers function is complete
         t_print("Join network finished!")
         return True
     
